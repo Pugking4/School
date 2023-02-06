ee10c8cc69b2d9bb1b9885feb7d04d120cc2d530
Pugking4 <joshuas13006@gmail.com>
2023-02-06 19:14:15 +1100

    Update UniqueInputNOCOUNT.py

--- Digital Technology/Python/UniqueInputNOCOUNT.py
+++ Digital Technology/Python/UniqueInputNOCOUNT.py
@@ -1,18 +1,26 @@
 #Challenge 1
 i = 0
 list = []
-list_add = []
+fail = 0
 userInput = input("Input: ")
 for c in userInput:
     list.append(c)
-    c = 0
 for c in userInput:
-    print(c)
-#    c += 1
-    if list == 1:
-        i += 1
-        if i == len(userInput):
-            print("Output: True")
-    else:
-        print("Output: False")
-        #break
+    x = c
+    i = 0
+    for c in userInput:
+        #print(x + c) #debug
+        if x == c:
+            fail += 1
+            #print("fail") #debug
+        else:
+            i += 1
+            #print(i) #debug
+    if i == 0:
+        print("True")
+#print(f"fail is at {fail}") #debug
+#print(f"fail thresh is {len(list)+1}") #debug
+if fail >= len(list)+1:
+    print("False")
+else:
+    print("True")
