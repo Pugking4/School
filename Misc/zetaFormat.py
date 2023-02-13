de63e632acc3e815399e532d7bd97e855ab2e2c9
Pugking4 <joshuas13006@gmail.com>
2023-02-14 01:28:16 +1100

    Updated zetaFormat.py

--- Misc/zetaFormat.py
+++ Misc/zetaFormat.py
@@ -3,69 +3,42 @@ import re
 _RE_COMBINE_WHITESPACE = re.compile(r"\s+")
 
 with open("Misc\\Zeta.txt",encoding="utf8") as f:
-    garbage = f.read()
-#garbage.splitlines()
+    unformatted_data = f.read()
 
-#or i in garbage:
-#print(garbage)
+remove_list = ['(煌)",','-','(煌)']
 
-#print(garbage)
-#garbageNew = ""
-#garbageNew = garbageNew.join(garbage.split())
-#print (garbageNew)
-
-garbageNew = _RE_COMBINE_WHITESPACE.sub("|", garbage).strip()
-#print(garbageNew)
-
-garbage_list = []
-garbage_list = garbageNew.strip("|").split("|")
-
-remove = ['(煌)",','-','(煌)']
-
-removeCount = 0
-for i in range(len(remove)):
-	removeCount = removeCount + garbage.count(remove[i])
-#removeCount = garbage.count(remove[0])
-#removeCount = removeCount + garbage.count(remove[1])
-
-z = 0
-print(f"{removeCount} remove")
-while z < removeCount:
-	for i in range(len(remove)):
-		if remove[i] in garbage_list:
-			garbage_list.remove(remove[i])
-	z += 1
-
-print(garbage_list)
-u = 2
-for i in range(len(garbage_list)//19):
-	#print(f"test {u}")
-	#print(garbage_list[u])
-	temp = garbage_list.index("DX")
-	res = garbage_list[u:temp]
-	garbage_list[u : temp] = [' '.join(garbage_list[u : temp])]
-	u += 19
-
-#garbage_list[2 : temp] = [' '.join(garbage_list[2 : temp])]
-
-
-
-
-#garbage_list.remove('(煌)",')
-#garbage_list.remove('-')
-#print(res)
-#print(garbage_list)
-print(len(garbage_list))
-print(garbage_list.count("FESTiVAL"))
-print(garbage_list.count("Splash+"))
-
-count = 0
+for i in range(len(remove_list)):
+	unformatted_data = unformatted_data.replace(remove_list[i],"")
+#Not written by me
+garbageNew = _RE_COMBINE_WHITESPACE.sub("|", unformatted_data).strip()
+#
+unformatted_list = []
+unformatted_list = garbageNew.strip("|").split("|")
+#Not written by me
+index_dx = [i for i, x in enumerate(unformatted_list) if x == "DX"]
+#
 x = 0
-y = 19
+with open("zeta_final.txt", 'w',encoding="utf8") as file_object:
+	for i in range(len(index_dx)):
+		file_object.write(f"{unformatted_list[x:index_dx[i]-3]}\n")
+		x = index_dx[i]-3
 
-while count < 44:
-	print(garbage_list[x:y]) 
-	#print("test")
-	x += 19
-	y += 19
-	count += 1
+#Not written by me
+lines = []
+with open("zeta_final.txt", 'r',encoding="utf8") as fp:
+    lines = fp.readlines()
+
+with open("zeta_final.txt", 'w',encoding="utf8") as fp:
+    for number, line in enumerate(lines):
+        if number not in [0]:
+            fp.write(line)
+#
+
+with open("zeta_final.txt", 'r',encoding="utf8") as file_object:
+	for lines in file_object:
+		lines = lines.replace('[',"")
+		lines = lines.replace(']',"")
+		lines = lines.replace("'","")
+		lines = lines.replace(" ","")
+		lines = lines.replace("_"," ")
+		print(lines.rstrip())
