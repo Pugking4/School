3df48e7b1d458aa3ea0de527aab57d51a7ca3a2a
Pugking4 <joshuas13006@gmail.com>
2023-02-14 13:29:29 +1100

    Updated zetaFormat.py

--- Misc/zetaFormat.py
+++ Misc/zetaFormat.py
@@ -1,11 +1,12 @@
+import os
 import re
-
 _RE_COMBINE_WHITESPACE = re.compile(r"\s+")
 
 with open("Misc\\Zeta.txt",encoding="utf8") as f:
     unformatted_data = f.read()
 
-remove_list = ['(煌)",','-','(煌)']
+
+remove_list = ['(煌)",','-','(煌)','(星)']
 
 for i in range(len(remove_list)):
 	unformatted_data = unformatted_data.replace(remove_list[i],"")
@@ -18,27 +19,31 @@ unformatted_list = garbageNew.strip("|").split("|")
 index_dx = [i for i, x in enumerate(unformatted_list) if x == "DX"]
 #
 x = 0
-with open("zeta_final.txt", 'w',encoding="utf8") as file_object:
+with open("Misc\\zeta_final_temp.txt", 'w',encoding="utf8") as file_object1:
 	for i in range(len(index_dx)):
-		file_object.write(f"{unformatted_list[x:index_dx[i]-3]}\n")
+		
+		file_object1.write(f"{unformatted_list[x:index_dx[i]-3]}\n")
 		x = index_dx[i]-3
+
+y = 6
+with open("Misc\\zeta_final.txt", 'w',encoding="utf8") as file_object_w, open("Misc\\zeta_final_temp.txt", 'r',encoding="utf8") as file_object_r:
+	lines = file_object_r.readlines()
+	#print(lines)
+	for lines in lines:
+		res = lines.strip('][').replace("'","").replace("\n","").replace("]","").split(', ')
+		del res[y:-1]
+		res = " ".join(res)
+		file_object_w.write(f"{res}\n")
+		#print(res)
 
 #Not written by me
 lines = []
-with open("zeta_final.txt", 'r',encoding="utf8") as fp:
+with open("Misc\\zeta_final.txt", 'r',encoding="utf8") as fp:
     lines = fp.readlines()
 
-with open("zeta_final.txt", 'w',encoding="utf8") as fp:
+with open("Misc\\zeta_final.txt", 'w',encoding="utf8") as fp:
     for number, line in enumerate(lines):
         if number not in [0]:
             fp.write(line)
 #
-
-with open("zeta_final.txt", 'r',encoding="utf8") as file_object:
-	for lines in file_object:
-		lines = lines.replace('[',"")
-		lines = lines.replace(']',"")
-		lines = lines.replace("'","")
-		lines = lines.replace(" ","")
-		lines = lines.replace("_"," ")
-		print(lines.rstrip())
+os.remove("Misc\\zeta_final_temp.txt") 
